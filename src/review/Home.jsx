36aaa6dbf0dba0d1dc8683a4362a88da2c9f4d33
Dulce rojas <dulcerojas698@gmail.com>
2023-09-26 00:51:10 -0300

    Armando portafolio

--- src/review/Home.jsx
+++ src/review/Home.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>Home</div>
+  return (<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Rojas Dulce</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Acerca de mi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Proyectos</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Contacto
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Gmail</a></li>
+            <li><a class="dropdown-item" href="#">GitHub</a></li>
+            <li><a class="dropdown-item" href="#">LinkedIn</a></li>
+           
+          </ul>
+        </li>
+       
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+<img src="https://www.mytaskpanel.com/wp-content/uploads/2022/01/bffhhhshh.webp" class="img-fluid" alt="imagen"></img></div>
   )
 }
 
